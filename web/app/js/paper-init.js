08b3cc45eca36eb18dce3ca15c9f617cbad4e682
Enriqe <info@brevistener.com>
2016-12-29 10:58:18 +0100

    Odstranene subory PAPER.JS

--- web/app/js/paper-init.js
+++
@@ -1,6 +0,0 @@
-window.onload = function() {
-    // Get a reference to the canvas object
-    var canvas = document.getElementById('mainCanvas');
-    // Create an empty project and a view for the canvas:
-    paper.setup(canvas);
-}
